04b6c721ee6239178c6e5dc8fff56795cc829066
Arjan van der Gaag <info@agwebdesign.nl>
2011-05-21 10:48:41 +0200

    Extracted logic into subclasses

--- lib/nanoc3/filters/cache_buster.rb
+++ lib/nanoc3/filters/cache_buster.rb
@@ -16,14 +16,15 @@ module Nanoc3
     class CacheBuster < Nanoc3::Filter
       identifier :cache_buster
 
-      # Regex for finding all references to files to be cache busted in CSS files
-      REGEX_CSS = /url\(('|"|)(([^'")]+)\.(#{Nanoc3::Cachebuster::FILETYPES_TO_FINGERPRINT.join('|')}))\1\)/i
-
-      # Regex for finding all references to files to be cache busted in HTML files
-      REGEX_HTML = /(href|src)=("|'|)([^'"]+(\.(?:#{Nanoc3::Cachebuster::FILETYPES_TO_FINGERPRINT.join('|')})))\2/
-
       def run(content, args = {})
-        stylesheet? ? bust_stylesheet(content) : bust_page(content)
+        strategy = stylesheet? ? Css.new(site, item) : Html.new(site, item)
+        content.gsub(strategy.class::REGEX) do |m|
+          begin
+            strategy.apply m, $1, $2, $3, $4
+          rescue Nanoc3::Cachebuster::NoCacheBusting, Nanoc3::Cachebuster::NoSuchSourceFile
+            m
+          end
+        end
       end
 
     private
@@ -43,111 +44,99 @@ module Nanoc3
         end.include?(@item[:extension].to_s)
       end
 
-      # Make a path relative to the site's content dir absolute
-      # This works just like File.join, but makes the end result relative
-      # to the project directory (the directory one level above this file).
-      #
-      # @param <String> path part of a path
-      # @param ...
-      # @return <String> path
-      def content_path(*path)
-        File.join(Dir.pwd, 'content', *path)
-      end
+      class Strategy
+        attr_reader :site, :item
 
-      # Add cache-busters to stylesheets
-      #
-      # @param <String> content of a stylesheet to rewrite
-      # @return <String> rewritten content
-      def bust_stylesheet(content)
-        content.gsub(REGEX_CSS) do |m|
-          quote, filename, basename, extension = $1, $2, $3, $4
-          begin
-            real_path = content_path(source_path(filename))
-            m.sub(filename, basename + Nanoc3::Cachebuster.fingerprint_file(real_path) + '.' + extension)
-          rescue Nanoc3::Cachebuster::NoCacheBusting, Nanoc3::Cachebuster::NoSuchSourceFile
-            m
+        def initialize(site, item)
+          @site, @item = site, item
+        end
+
+        def fingerprint(filename)
+          Nanoc3::Cachebuster.fingerprint_file content_path(source_path(filename))
+        end
+
+        # Make a path relative to the site's content dir absolute
+        # This works just like File.join, but makes the end result relative
+        # to the project directory (the directory one level above this file).
+        #
+        # @param <String> path part of a path
+        # @param ...
+        # @return <String> path
+        def content_path(*path)
+          File.join(Dir.pwd, 'content', *path)
+        end
+
+        # Try to find the source path of a referenced file.
+        #
+        # This will use Nanoc's routing rules to try and find an item whose output
+        # path matches the path given, which is a source reference. It returns
+        # the path to the content file if a match is found.
+        #
+        # As an example, when we use the input file "assets/styles.scss" for our
+        # stylesheet, then we refer to that file in our HTML as "assets/styles.css".
+        # Given the output filename, this method will return the input filename.
+        #
+        # @raises NoSuchSourceFile when no match is found
+        # @param <String> path is the reference to an asset file from another source
+        #   file, such as '/assets/styles.css'
+        # @return <String> the path to the content file for the referenced file,
+        #   such as '/assets/styles.scss'
+        def source_path(path)
+          path = absolutize(path)
+
+          matching_item = site.items.find do |i|
+            i.path.sub(/-cb[a-zA-Z0-9]{9}(?=\.)/, '') == path
           end
+
+          # Make sure the reference is left alone if we cannot find a source file
+          # to base the fingerprint on, or if the file for some reason has no
+          # cache busting applied to it (for example, when it is manually overriden
+          # in the Rules file.)
+          raise Nanoc3::Cachebuster::NoSuchSourceFile, 'no source file found matching ' + path unless matching_item
+          raise Nanoc3::Cachebuster::NoCacheBusting, 'there is no cache busting applied to ' + matching_item.identifier unless matching_item.path =~ /-cb[a-zA-Z0-9]{9}(?=\.)/
+
+          # Return the path to the source file in question without the starting content
+          # part, since that is added by #content_path
+          matching_item[:content_filename].sub(/^content\//, '')
+        end
+
+        # Get the absolute path to a file, whereby absolute means relative to the root.
+        #
+        # When we are trying to get to a source file via a referenced filename,
+        # that filename may be absolute (relative to the site root) or relative to
+        # the file itself. In the latter case, our file detection would miss it.
+        # We therefore rewrite any reference not starting with a forward slash
+        # to include the full path of the referring item.
+        #
+        # @example Using an absolute input path in 'assets/styles.css'
+        #   absolutize('/assets/logo.png') # => '/assets/logo.png'
+        # @example Using a relative input path in 'assets/styles.css'
+        #   absolutize('logo.png') # => '/assets/logo.png'
+        #
+        # @param <String> path is the path of the file that is referred to in
+        #   an input file, such as a stylesheet or HTML page.
+        # @return <String> path to the same file as the input path but relative
+        #   to the site root.
+        def absolutize(path)
+          return path if path =~ /^\//
+          File.join(File.dirname(item[:content_filename]), path).sub(/^content/, '')
         end
       end
 
-      # Add cache-busters to HTML pages.
-      #
-      # Since not every file referenced in the output is an actual input item,
-      # we use {@link source_path} to get to the original input item for that
-      # reference.
-      #
-      # If we cannot find an input item for the given reference, we leave the
-      # reference alone. If we do, we add a cache buster to its filename.
-      #
-      # @see #source_path
-      # @param <String> content of a page to rewrite
-      # @return <String> rewritten content
-      def bust_page(content)
-        content.gsub(REGEX_HTML) do |m|
-          attribute, quote, path, extension = $1, $2, $3, $4
-          begin
-            real_path = content_path(source_path(path))
-            %Q{#{attribute}=#{quote}#{path.sub(extension, Nanoc3::Cachebuster.fingerprint_file(real_path) + extension)}#{quote}}
-          rescue Nanoc3::Cachebuster::NoSuchSourceFile, Nanoc3::Cachebuster::NoCacheBusting
-            m
-          end
+      class Css < Strategy
+        REGEX = /url\(('|"|)(([^'")]+)\.(#{Nanoc3::Cachebuster::FILETYPES_TO_FINGERPRINT.join('|')}))\1\)/i
+
+        def apply(m, quote, filename, basename, extension)
+          m.sub(filename, basename + fingerprint(filename) + '.' + extension)
         end
       end
 
-      # Try to find the source path of a referenced file.
-      #
-      # This will use Nanoc's routing rules to try and find an item whose output
-      # path matches the path given, which is a source reference. It returns
-      # the path to the content file if a match is found.
-      #
-      # As an example, when we use the input file "assets/styles.scss" for our
-      # stylesheet, then we refer to that file in our HTML as "assets/styles.css".
-      # Given the output filename, this method will return the input filename.
-      #
-      # @raises NoSuchSourceFile when no match is found
-      # @param <String> path is the reference to an asset file from another source
-      #   file, such as '/assets/styles.css'
-      # @return <String> the path to the content file for the referenced file,
-      #   such as '/assets/styles.scss'
-      def source_path(path)
-        path = absolutize(path)
+      class Html < Strategy
+        REGEX = /(href|src)=("|'|)([^'"]+(\.(?:#{Nanoc3::Cachebuster::FILETYPES_TO_FINGERPRINT.join('|')})))\2/
 
-        matching_item = @items.find do |item|
-          item.path.sub(/-cb[a-zA-Z0-9]{9}(?=\.)/, '') == path
+        def apply(m, attribute, quote, filename, extension)
+          %Q{#{attribute}=#{quote}#{filename.sub(extension, fingerprint(filename) + extension)}#{quote}}
         end
-
-        # Make sure the reference is left alone if we cannot find a source file
-        # to base the fingerprint on, or if the file for some reason has no
-        # cache busting applied to it (for example, when it is manually overriden
-        # in the Rules file.)
-        raise Nanoc3::Cachebuster::NoSuchSourceFile, 'no source file found matching ' + path unless matching_item
-        raise Nanoc3::Cachebuster::NoCacheBusting, 'there is no cache busting applied to ' + matching_item.identifier unless matching_item.path =~ /-cb[a-zA-Z0-9]{9}(?=\.)/
-
-        # Return the path to the source file in question without the starting content
-        # part, since that is added by #content_path
-        matching_item[:content_filename].sub(/^content\//, '')
-      end
-
-      # Get the absolute path to a file, whereby absolute means relative to the root.
-      #
-      # When we are trying to get to a source file via a referenced filename,
-      # that filename may be absolute (relative to the site root) or relative to
-      # the file itself. In the latter case, our file detection would miss it.
-      # We therefore rewrite any reference not starting with a forward slash
-      # to include the full path of the referring item.
-      #
-      # @example Using an absolute input path in 'assets/styles.css'
-      #   absolutize('/assets/logo.png') # => '/assets/logo.png'
-      # @example Using a relative input path in 'assets/styles.css'
-      #   absolutize('logo.png') # => '/assets/logo.png'
-      #
-      # @param <String> path is the path of the file that is referred to in
-      #   an input file, such as a stylesheet or HTML page.
-      # @return <String> path to the same file as the input path but relative
-      #   to the site root.
-      def absolutize(path)
-        return path if path =~ /^\//
-        File.join(File.dirname(item[:content_filename]), path).sub(/^content/, '')
       end
     end
   end
