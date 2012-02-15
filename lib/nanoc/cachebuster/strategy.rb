06aafb41fdc3d4b8d501b1d4bb98dc5d35223bbc
Arjan van der Gaag <arjan@kabisa.nl>
2012-02-15 20:26:10 +0100

    Revert "replaces join with expand_path, adds comments to look into rationale behind edge case test"
    This reverts commit 49b1211cb4c44303e595e4b9df6c407d3f82fc2a because it
    introduced a failing spec.

--- lib/nanoc/cachebuster/strategy.rb
+++ lib/nanoc/cachebuster/strategy.rb
@@ -110,7 +110,7 @@ module Nanoc
         if current_item[:content_filename]
           File.expand_path(File.join(File.dirname(current_item[:content_filename]), path)).sub(/^#{Dir.pwd}/, '').sub(/^\/content/, '')
         else
-          File.expand_path(File.dirname(current_item.path), path)
+          File.join(File.dirname(current_item.path), path)
         end
       end
     end
